34fc4f07bcb60a2e8f55c108bd0ad8b2aa57dec7
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2021-09-16 20:43:01 +0530

    In retrospect, it was inevitable

---
+++ README.md
@@ -0,0 +1,44 @@
+# BitClout.py
+
+A python package for bitclout.
+
+Developed by [ItsAditya](https://bitclout.com/itsaditya)
+
+## Examples of How To Use BitClout.py
+
+Getting $CLOUT price
+```python
+from bitclout import *
+print(getCloutPrice())
+```
+
+Getting user(s) info through publicKey(s)
+```python
+from bitclout import *
+import json
+with open("userInfo.json", "w") as file:
+    listOfPublicKeys = ["BC1YLjJVhcVAi5UCYZ2aTNwRMtqvzQar4zbymr7fyinu8MsWLx2A1g1"] # you can pass multiple public key of users
+    json.dump(getUserStateless(listOfPublicKeys))
+```
+
+Getting user info through BitClout username
+```python
+from bitclout import *
+import json
+with open("userInfo.json", "w") as file:
+    username = "ItsAditya" 
+    json.dump(getSingleProfile(username))
+
+```
+
+Getting users who are blocked by a profile
+```python
+from bitclout import *
+import json
+with open("blockedUsers.json", "w") as file:
+    publicKey = "BC1YLhBLE1834FBJbQ9JU23JbPanNYMkUsdpJZrFVqNGsCe7YadYiUg" # well, that's my (@ItsAditya) public key :)
+    json.dump(getUsersBlocked(publicKey))
+```
+
+
+More docs coming soon!
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="vidstream",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
