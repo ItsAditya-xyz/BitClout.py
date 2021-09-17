c58610bbda6091936d85aa83b755d6b0d3d7491e
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2021-09-17 17:27:24 +0530

    added diamond methods

--- test.py
+++
@@ -1,8 +0,0 @@
-import bitclout
-import json
-with open("diamondsReceived.json", "w") as file:
-    publicKey = "BC1YLhBLE1834FBJbQ9JU23JbPanNYMkUsdpJZrFVqNGsCe7YadYiUg"
-    diamondData = bitclout.Diamonds.getDiamonds(publicKey=publicKey, received=True)
-    '''reveived is an optional arguement when true it returns diamond received by users else
-    return diamonds give by users'''
-    json.dump(diamondData, file)
